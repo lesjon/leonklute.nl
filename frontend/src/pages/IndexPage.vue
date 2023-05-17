192b2083f6c36c664e397fe7d6a6dc885f07c9a4
Klute <l.klute@caesar.nl>
2023-05-17 09:41:01 +0200

    main: basic frontend with picture

---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <photo-reel />
+  </q-page>
+</template>
+
+<script lang="ts">
+import PhotoReel from 'components/PhotoReel.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { PhotoReel }
+});
+</script>
