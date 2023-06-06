37d2f42fa89d2734bba38061163dc1d81bbe107f
Leon Klute <l.klute@caesar.nl>
2023-06-06 15:55:28 +0200

    chess: overhaul movement

--- frontend/src/components/chess-pieces.ts
+++ frontend/src/components/chess-pieces.ts
@@ -48,12 +48,19 @@ export const fen2FullName = (fen: string) => {
     }
 }
 
+interface ChessPieceStep {
+    row: number;
+    column: number;
+    limit?: number;
+}
+
 export interface ChessPiece {
     getFullName(): string;
     getFenKey(): string;
     getColor(): string;
     isOpponent(piece: ChessPiece): boolean;
     getPossibleMoves(position: Square, enPassant?: EnPassant): Square[];
+    getChessPieceSteps(): ChessPieceStep[];
 }
 
 export const ChessPieceFromFen = (fenKey: FENpieces): ChessPiece | null => {
@@ -90,20 +97,20 @@ export const ChessPieceFromFen = (fenKey: FENpieces): ChessPiece | null => {
 export function isOpponentFen(fromFen: FENpieces, toFen: FENpieces): boolean {
     const fromPiece = ChessPieceFromFen(fromFen);
     const toPiece = ChessPieceFromFen(toFen);
-    if(!fromPiece || !toPiece){
+    if (!fromPiece || !toPiece) {
         return false;
     }
     return fromPiece?.isOpponent(toPiece);
 }
 
-abstract class ChessPieceBase implements ChessPiece{
+abstract class ChessPieceBase implements ChessPiece {
     fullName: string;
     fenKey: string;
     color: string;
-    constructor(fenKey: string){
+    constructor(fenKey: string) {
         this.fullName = fen2FullName(fenKey);
         this.fenKey = fenKey;
-        this.color = fenKey === fenKey.toUpperCase() ? 'w' : 'b';        
+        this.color = fenKey === fenKey.toUpperCase() ? 'w' : 'b';
     }
     getFullName(): string {
         return this.fullName;
@@ -117,93 +124,73 @@ abstract class ChessPieceBase implements ChessPiece{
     isOpponent(piece: ChessPiece): boolean {
         return this.color !== piece.getColor();
     }
-    abstract getPossibleMoves(position: Square): Square[];
-}
-
-function moveStraight(position: Square): Square[]{
-    const possibleMoves: Square[] = [];
-    possibleMoves.push(...Array.from(rows, (v) => ({row: v, column: position.column})));
-    possibleMoves.push(...Array.from(columns, (v) => ({row: position.row, column: v})));
-    return possibleMoves;
-}
-
-function moveDiagonally(position: Square): Square[]{
-    const possibleMoves: Square[] = [];
-    const rowsBack = Array.from(rows, (v, i) => -v);
-    const allRows = [...rowsBack, ...rows];
-    const columnsBack = Array.from(columns, (v, i) => -v);
-    const allColumns = [...columnsBack, ...columns];
-    for(let i = 0; i < allRows.length; i++){
-        possibleMoves.push({row: position.row + allRows[i], column: position.column + allColumns[i]});
-        possibleMoves.push({row: position.row + allRows[i], column: position.column - allColumns[i]});
-    }
-    return possibleMoves;
-}
-
-class Rook extends ChessPieceBase{
     getPossibleMoves(position: Square): Square[] {
-        const possibleMoves = moveStraight(position);
-        possibleMoves.filter(move => move === position)
+        const possibleMoves: Square[] = [];
+        this.getChessPieceSteps().forEach(step => {
+            console.log('step', step);
+            for (let i = 0; i < (step.limit || 8); i++) {
+                possibleMoves.push({ row: position.row + ((i+1)*step.row), column: position.column + ((i+1)*step.column) });
+            }
+        });
         return possibleMoves;
     }
-
-    constructor(fenKey: string){
-        super(fenKey);
-    }
-}
-class King extends ChessPieceBase{
-    constructor(fenKey: string){
-        super(fenKey);
-    }
-    getPossibleMoves(position: Square): Square[] {
-        let possibleMoves: Square[] = [];
-        const deltas = [-1, 0, 1];
-        possibleMoves = Array.from(deltas, (v) => Array.from(deltas, (w) => ({row: position.row + v, column: position.column + w}))).flat();
-        possibleMoves.filter(move => move === position)
-        return possibleMoves;
-    }
-}
-class Queen extends ChessPieceBase{
-    constructor(fenKey: string){
-        super(fenKey);
-    }
-    getPossibleMoves(position: Square): Square[] {
-        const possibleMoves = moveStraight(position);
-        possibleMoves.push(...moveDiagonally(position));
-        possibleMoves.filter(move => move === position);
-        return possibleMoves;
-    }
-}
-class Knight extends ChessPieceBase{
-    constructor(fenKey: string){
-        super(fenKey);
-    }
-    getPossibleMoves(position: Square): Square[] {
-        return [];
-    }
-}
-class Bishop extends ChessPieceBase{
-    constructor(fenKey: string){
-        super(fenKey);
-    }
-    getPossibleMoves(position: Square): Square[] {
-        const possibleMoves = moveDiagonally(position);
-        possibleMoves.filter(move => move === position);
-        return possibleMoves;
-    }
+    abstract getChessPieceSteps(): ChessPieceStep[];
 }
 
-class Pawn extends ChessPieceBase{
-    constructor(fenKey: string){
+class Rook extends ChessPieceBase {
+    constructor(fenKey: string) {
         super(fenKey);
     }
 
-    computeEnPassant(position: Square, enPassant?: Square): Square | undefined{
-        if (enPassant){
+    getChessPieceSteps(): ChessPieceStep[] {
+        return [{ row: 0, column: 1 }, { row: 1, column: 0 }, { row: 0, column: -1 }, { row: -1, column: 0 }];
+    }
+}
+class King extends ChessPieceBase {
+    constructor(fenKey: string) {
+        super(fenKey);
+    }
+    getChessPieceSteps(): ChessPieceStep[] {
+        return [{ row: 0, column: 1, limit: 1 }, { row: -1, column: 1, limit: 1 }, { row: -1, column: 0, limit: 1 }, { row: -1, column: -1, limit: 1 }, { row: 0, column: -1, limit: 1 }, { row: 1, column: -1, limit: 1 }, { row: 1, column: 0, limit: 1 }, { row: 1, column: 1, limit: 1 }];
+    }
+}
+class Queen extends ChessPieceBase {
+    constructor(fenKey: string) {
+        super(fenKey);
+    }
+    getChessPieceSteps(): ChessPieceStep[] {
+        return [{ row: 0, column: 1 }, { row: -1, column: 1 }, { row: -1, column: 0 }, { row: -1, column: -1 }, { row: 0, column: -1 }, { row: 1, column: -1 }, { row: 1, column: 0 }, { row: 1, column: 1 }];
+    }
+}
+class Knight extends ChessPieceBase {
+    constructor(fenKey: string) {
+        super(fenKey);
+    }
+    getChessPieceSteps(): ChessPieceStep[] {
+        return [{ row: 2, column: -1, limit: 1 }, { row: 1, column: -2, limit: 1 }, { row: -1, column: -2, limit: 1 }, { row: -2, column: -1, limit: 1 }, { row: -2, column: 1, limit: 1 }, { row: -1, column: 2, limit: 1 }, { row: 1, column: 2, limit: 1 }, { row: 2, column: 1, limit: 1 }];
+    }
+}
+
+class Bishop extends ChessPieceBase {
+    constructor(fenKey: string) {
+        super(fenKey);
+    }
+    getChessPieceSteps(): ChessPieceStep[] {
+        return [{ row: 1, column: -1}, { row: -1, column: -1 }, { row: -1, column: 1 }, { row: 1, column: 1}];
+    }
+}
+
+class Pawn extends ChessPieceBase {
+    constructor(fenKey: string) {
+        super(fenKey);
+    }
+
+    computeEnPassant(position: Square, enPassant?: Square): Square | undefined {
+        if (enPassant) {
             console.log('enPassant', enPassant);
             const adjacentColumn = position.column === enPassant.column + 1 || position.column === enPassant.column - 1;
             const correctRow = this.color === 'w' ? position.row === 5 : position.row === 4;
-            if (adjacentColumn && correctRow){
+            if (adjacentColumn && correctRow) {
                 return enPassant;
             }
         }
@@ -214,22 +201,26 @@ class Pawn extends ChessPieceBase{
         console.log('getPossibleMoves', position, enPassant);
         const possibleMoves: Square[] = [];
         const enPassantMove = this.computeEnPassant(position, enPassant);
-        if (enPassantMove){
+        if (enPassantMove) {
             possibleMoves.push(enPassantMove);
         }
 
-        if(this.color === 'w'){
+        if (this.color === 'w') {
             if (position.row === 2) {
-                possibleMoves.push({row: position.row + 2, column: position.column});
+                possibleMoves.push({ row: position.row + 2, column: position.column });
             }
-            possibleMoves.push({row: position.row + 1, column: position.column});
+            possibleMoves.push({ row: position.row + 1, column: position.column });
         } else {
             if (position.row === 7) {
-                possibleMoves.push({row: position.row - 2, column: position.column});
+                possibleMoves.push({ row: position.row - 2, column: position.column });
             }
-            possibleMoves.push({row: position.row - 1, column: position.column});
+            possibleMoves.push({ row: position.row - 1, column: position.column });
         }
         console.log('possibleMoves', possibleMoves);
         return possibleMoves;
+    }
+
+    getChessPieceSteps(): ChessPieceStep[] {
+        return [{ row: 1, column: 0, limit: 1 }];
     }
 }
