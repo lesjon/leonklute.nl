0a1fd9d061934ead11cf1da0155075ba410511f2
Leon Klute <l.klute@caesar.nl>
2023-06-08 14:52:03 +0200

    chess: move controls to component

---
+++ frontend/src/components/ChessControls.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-card>
+    <q-card-actions>
+      <q-btn class="q-mr-md" color="primary" label="New Game" @click="game?.newGame()" />
+      <q-btn class="q-mr-md" color="primary" label="Flip Board" @click="$emit('flip')" />
+    </q-card-actions>
+    <q-card-section @click="copyFen" style="width: 90ch;">
+      <q-input outlined readonly dense :model-value="game?.toFen()" />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { copyToClipboard } from 'quasar';
+import { defineComponent, PropType } from 'vue';
+import ChessGame from './chess-game';
+
+export default defineComponent({
+  name: 'ChessControls',
+  props: {
+    game: {
+      type: Object as PropType<ChessGame>,
+    }
+  },
+  emits: ['flip'],
+  methods: {
+    failedToCopy() {
+      this.$q.notify({
+        message: 'Failed to copy FEN to clipboard',
+        color: 'negative',
+      });
+    },
+    copyFen() {
+      if (!this.game) {
+        this.failedToCopy();
+        return;
+      }
+      copyToClipboard(this.game.toFen()).then(() => {
+        this.$q.notify({
+          message: 'FEN copied to clipboard',
+          color: 'positive',
+        })
+      }).catch(this.failedToCopy);
+    },
+  },
+})
+</script>
--- frontend/src/pages/AnalysisPage.vue
+++ frontend/src/pages/AnalysisPage.vue
@@ -1,28 +1,20 @@
 <template>
   <q-page padding class="row justify-center q-gutter-lg">
     <chess-board :game="game" :flipped="flipped" class="col-12" />
-    <q-card style="max-width: 90vw; width: 90ch;">
-      <q-card-actions>
-        <q-btn class="q-mr-md" color="primary" label="New Game" @click="game?.newGame()" />
-        <q-toggle class="q-mr-md" color="primary" label="Flip Board" v-model="flipped" />
-      </q-card-actions>
-      <q-card-section @click="copyFen">
-        <q-input outlined readonly dense :model-value="game.toFen()" />
-      </q-card-section>
-    </q-card>
+    <chess-controls :game="game" @flip="flipped = !flipped" style="max-width: 90vw;" />
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { copyToClipboard } from 'quasar';
+import ChessControls from 'src/components/ChessControls.vue';
 import ChessBoard from 'src/components/ChessBoard.vue';
 import ChessGame from 'src/components/chess-game';
 
 export default defineComponent({
   name: 'AnalysisPage',
   components: {
-    ChessBoard,
+    ChessBoard, ChessControls
   },
   data() {
     const game = new ChessGame();
@@ -30,21 +22,6 @@ export default defineComponent({
       game,
       flipped: false,
     };
-  },
-  methods: {
-    copyFen() {
-      copyToClipboard(this.game.toFen()).then(() => {
-        this.$q.notify({
-          message: 'FEN copied to clipboard',
-          color: 'positive',
-        })
-      }).catch(() => {
-        this.$q.notify({
-          message: 'Failed to copy FEN to clipboard',
-          color: 'negative',
-        });
-      })
-    },
-  },
+  }
 })
 </script>
