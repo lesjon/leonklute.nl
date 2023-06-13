575e3b4ce3564a0531bf5b531325c9a42cbd9bbc
Leon Klute <l.klute@caesar.nl>
2023-06-13 12:57:08 +0200

    chess: create promotionPicker

---
+++ frontend/src/components/PromotionPicker.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-responsive ratio="1">
+    <div style="display: grid;" v-if="playerColor === 'w'">
+      <q-img loading="eager" :src="WhiteQueen" style="grid-column: 1; grid-row: 1;" @click="$emit('select', ChessPieceType.whiteQueen)"/>
+      <q-img loading="eager" :src="WhiteRook" style="grid-column: 1; grid-row: 2;" @click="$emit('select', ChessPieceType.whiteRook)"/>
+      <q-img loading="eager" :src="WhiteBishop" style="grid-column: 2; grid-row: 1;" @click="$emit('select', ChessPieceType.whiteBishop)"/>
+      <q-img loading="eager" :src="WhiteKnight" style="grid-column: 2; grid-row: 2;" @click="$emit('select', ChessPieceType.whiteKnight)"/>
+    </div>
+    <div style="display: grid;" v-else>
+      <q-img loading="eager" :src="BlackQueen" style="grid-column: 1; grid-row: 1;" @click="$emit('select', ChessPieceType.blackQueen)"/>
+      <q-img loading="eager" :src="BlackRook" style="grid-column: 1; grid-row: 2;" @click="$emit('select', ChessPieceType.blackRook)"/>
+      <q-img loading="eager" :src="BlackBishop" style="grid-column: 2; grid-row: 1;" @click="$emit('select', ChessPieceType.blackBishop)"/>
+      <q-img loading="eager" :src="BlackKnight" style="grid-column: 2; grid-row: 2;" @click="$emit('select', ChessPieceType.blackKnight)"/>
+    </div>
+  </q-responsive>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { ChessPieceType, PlayerColor } from './chess-pieces';
+import WhiteQueen from 'assets/pieces/queen_white.png';
+import WhiteRook from 'assets/pieces/rook_white.png';
+import WhiteBishop from 'assets/pieces/bishop_white.png';
+import WhiteKnight from 'assets/pieces/knight_white.png';
+import BlackQueen from 'assets/pieces/queen_black.png';
+import BlackRook from 'assets/pieces/rook_black.png';
+import BlackBishop from 'assets/pieces/bishop_black.png';
+import BlackKnight from 'assets/pieces/knight_black.png';
+
+export default defineComponent({
+  name: 'PromotionPicker',
+  props: {
+    playerColor: {
+      type: String as PropType<PlayerColor>,
+      required: false,
+    }
+  },
+  emits: ["select"],
+  data() {
+    return {
+      ChessPieceType,
+      WhiteQueen,
+      WhiteRook,
+      WhiteBishop,
+      WhiteKnight,
+      BlackQueen,
+      BlackRook,
+      BlackBishop,
+      BlackKnight,
+    };
+  },
+})
+</script>
